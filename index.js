317b7ff8bad8418c7780708e221cace5cd87d420
savita madray <savitamadray01@gmail.com>
2017-09-30 00:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
+  var animal='cat'  // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return myAnimal()
+  return animal
 
 }
 
